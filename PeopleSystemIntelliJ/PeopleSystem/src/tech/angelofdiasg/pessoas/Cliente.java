8ab7510ad306e2802c4dce3d826683ff4bd89010
Angelo Francescoly Dias Gonçalves <angelo.dias@academicos.com>
2024-10-21 21:03:30 -0300

    Códigos da aula 21/10/2024.

--- PeopleSystemIntelliJ/PeopleSystem/src/tech/angelofdiasg/pessoas/Cliente.java
+++ PeopleSystemIntelliJ/PeopleSystem/src/tech/angelofdiasg/pessoas/Cliente.java
@@ -2,6 +2,7 @@ package tech.angelofdiasg.pessoas;
 
 import tech.angelofdiasg.composicao.Profissao;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Cliente extends Pessoa{
@@ -9,6 +10,8 @@ public class Cliente extends Pessoa{
     private Profissao profissao;
     private String tipoProfissaoOutros;
     private Scanner leitor = new Scanner(System.in);
+
+
 
     public String getCodigo() {
         return codigo;
@@ -35,6 +38,59 @@ public class Cliente extends Pessoa{
         if(profissao == Profissao.OUTROS){
             System.out.println(("Informe sua profissão: "));
             this.tipoProfissaoOutros = leitor.nextLine();
+        }
+
+    }
+
+    public void atualizarInformacoes(){
+        System.out.println("Deseja atualizar as informações pessoais? (S/N)");
+        String resposta = leitor.nextLine();
+        if(resposta.equalsIgnoreCase("S")){
+            System.out.println("Informe o nome atualizado: ");
+            this.setNome(leitor.nextLine());
+
+            System.out.println("Deseja atualizar a data de nascimento? (S/N)");
+            resposta = leitor.nextLine();
+            if(resposta.equalsIgnoreCase("S")){
+                System.out.println("Informe o dia de nascimento: ");
+                int dia = leitor.nextInt();
+                System.out.println("Informe o mês de nascimento: ");
+                int mes = leitor.nextInt();
+                System.out.println("Informe o ano de nascimento: ");
+                int ano = leitor.nextInt();
+                leitor.nextLine();
+                this.setDataNascimento(LocalDate.of(ano, mes, dia));
+            }
+            System.out.println("Deseja atualizar o endereço? (S/N)");
+            resposta = leitor.nextLine();
+            if (resposta.equalsIgnoreCase("S")) {
+                this.getEndereco().cadastrar();
+            }
+            System.out.println("Deseja atualizar o telefone de contato? (S/N)");
+            resposta = leitor.nextLine();
+            if (resposta.equalsIgnoreCase("S")) {
+                this.getTelsContato().cadastrar();
+            }
+        }
+        System.out.println("Deseja atualizar o código do cliente? (S/N)");
+        resposta = leitor.nextLine();
+        if (resposta.equalsIgnoreCase("S")) {
+            System.out.println("Informe o novo código: ");
+            this.codigo = leitor.nextLine();
+        }
+        System.out.println("Deseja atualizar a profissão do cliente? (S/N)");
+        resposta = leitor.nextLine();
+        if (resposta.equalsIgnoreCase("S")) {
+            System.out.println("Informe a nova profissão: ");
+            Profissao novaProfissao = Profissao.valueOf(leitor.nextLine());
+            this.setProfissao(novaProfissao);
+
+            if (novaProfissao == Profissao.OUTROS) {
+                System.out.println("Informe a nova profissão personalizada: ");
+                this.tipoProfissaoOutros = leitor.nextLine();
+            } else {
+                this.tipoProfissaoOutros = null;
+            }
         }
 
     }
